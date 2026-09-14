9224f2d86f570e6ea89e315eee507b9beaa18185
pacizio <pacizio60@gmail.com>
2026-09-14 12:08:51 +0100

    iyad

--- api/index.php
+++ api/index.php
@@ -215,7 +215,7 @@
             <li><a href="#skills">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
-            <li><a href="modules.html">Modules</a></li>
+            <li><a href="/modules.html">Modules</a></li>
         </ul>
 
     </nav>
